dc510bdecea92907195c929c33664ee4012f3f34
James Gatheru <jamesgatheru9905@gmail.com>
2024-08-21 01:07:43 +0300

    final push before deployment

--- flaskblog/config.py
+++ flaskblog/config.py
@@ -1,14 +1,19 @@
 import os
+from dotenv import load_dotenv
+
+# Load environment variables from the .env file
+load_dotenv()
 
 class Config:
-    SECRET_KEY = os.environ.get('SECRET_KEY')
-    SQLALCHEMY_DATABASE_URI = os.environ.get('SQLALCHEMY_DATABASE_URI')
-    MAIL_SERVER = os.environ.get('MAIL_SERVER')
+    SECRET_KEY = os.getenv('SECRET_KEY')
+    SQLALCHEMY_DATABASE_URI = os.getenv('SQLALCHEMY_DATABASE_URI')
+    MAIL_SERVER = os.getenv('MAIL_SERVER')
     MAIL_PORT = 587
     MAIL_USE_TLS = True
     MAIL_USE_SSL = False
-    MAIL_USERNAME = os.environ.get('EMAIL_USER')   
-    MAIL_PASSWORD = os.environ.get('EMAIL_PASS')
+    MAIL_USERNAME = os.getenv('EMAIL_USER')
+    MAIL_PASSWORD = os.getenv('EMAIL_PASS')
+
 
     # export  EMAIL_USER="mojay9905@gmail.com"
     # export  EMAIL_PASS="James9905"
